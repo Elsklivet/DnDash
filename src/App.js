cdabe50dfcfe5fd5eba001ae0afac6edd2d466bf
Elsklivet <gavinmajetich@gmail.com>
2022-09-25 19:12:02 -0400

    Finally added the add subcard boxes

--- src/App.js
+++ src/App.js
@@ -3,25 +3,129 @@ import './App.scss';
 import React, { useState, useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
 
 function MonsterCard() {
+
+  function AddMonsterCard() {
+    return (
+      <div className='AddMonsterCard'>
+        <div style={{'grid-area': 'name'}}>
+          <label>Name: </label>
+          <input type='text' style={{'width': '85%'}}></input>
+        </div>
+        <div style={{'grid-area': 'hp'}}>
+          <label>HP: </label>
+          <input type='text'></input>
+        </div>
+        <div style={{'grid-area': 'xp'}}>
+          <label>XP: </label>
+          <input type='text'></input>
+        </div>
+        <div style={{'grid-area': 'ac'}}>
+          <label>AC: </label>
+          <input type='text'></input>
+        </div>
+        <div style={{'grid-area': 'init'}}>
+          <label>Init: </label>
+          <input type='text'></input>
+        </div>
+        <div style={{'grid-area': 'stag'}}>
+          <label>Stagger: </label>
+          <input type='text'></input>
+          <input type='text' style={{'max-width': '60px', 'margin-left':'5px'}}></input>
+        </div>
+        <div style={{'grid-area': 'spell'}}>
+          <label>Spell Slots: </label>
+          <input type='text' style={{'width':'70%'}}></input>
+        </div>
+        <div style={{'grid-area': 'status'}}>
+          <label>Status: </label>
+          <input type='text' style={{'width':'90%'}}></input>
+        </div>
+        <div style={{'grid-area': 'url'}}>
+          <label>Details URL: </label>
+          <input type='text' style={{'width':'84%'}}></input>
+        </div>
+        <div style={{'grid-area': 'btn'}}>
+          <button className='Button' style={{'width':'50%'}}>Add</button>
+          <button className='Button' style={{'width':'50%'}}>Cancel</button>
+        </div>
+      </div>
+    )
+  }
+  
   return (
     <div className='MonsterCard'>
-      Monster Card
+      <h3>Monster Card</h3>
+      <AddMonsterCard></AddMonsterCard>
     </div>
   )
 }
 
 function LootCard() {
+
+  function AddLootCard() {
+    return (
+      <div className='AddLootCard'>
+        <div style={{'grid-area': 'name'}}>
+          <label>Name: </label>
+          <input type='text' style={{'width': '90%'}}></input>
+        </div>
+        <div style={{'grid-area': 'url'}}>
+          <label>Details URL: </label>
+          <input type='text' style={{'width':'84%'}}></input>
+        </div>
+        <div style={{'grid-area': 'gp'}}>
+          <label>GP: </label>
+          <input type='text' style={{'width':'85%'}}></input>
+        </div>
+        <div style={{'grid-area': 'class'}}>
+          <label>Class: </label>
+          <input type='text' style={{'width':'85%'}}></input>
+        </div>
+        <div style={{'grid-area': 'desc'}}>
+          <label>Description: </label>
+          <input type='text' style={{'width':'84%'}}></input>
+        </div>
+        <div style={{'grid-area': 'btn'}}>
+          <button className='Button' style={{'width':'50%'}}>Add</button>
+          <button className='Button' style={{'width':'50%'}}>Cancel</button>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className='LootCard'>
-      Loot Card
+      <h3>Loot Card</h3>
+      <AddLootCard></AddLootCard>
     </div>
   )
 }
 
 function TurnListCard() {
+
+  function AddTurnCard() {
+    return (
+      <div className='AddTurnCard'>
+        <div style={{'grid-area': 'name'}}>
+          <label>Name: </label>
+          <input type='text' style={{'width': '90%'}}></input>
+        </div>
+        <div style={{'grid-area': 'init'}}>
+          <label >Init: </label>
+          <input type='text' style={{'width': '93.3%'}}></input>
+        </div>
+        <div style={{'grid-area': 'btn'}}>
+          <button className='Button' style={{'width':'50%'}}>Add</button>
+          <button className='Button' style={{'width':'50%'}}>Cancel</button>
+        </div>
+      </div>
+    )
+  }
+  
   return (
     <div className='TurnListCard'>
-      Turn List Card
+      <h3>Turn List Card</h3>
+      <AddTurnCard></AddTurnCard>
     </div>
   )
 }
